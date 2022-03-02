2ed71a9b14baf1ffd29b3b7bfb11700f088b707c
katjz <katjazoner@gmail.com>
2022-03-02 15:34:45 -0500

    Remove --containall to avoid space issues

--- examples/launch_singularity.sh
+++ examples/launch_singularity.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Run singularity container interactively
-singularity shell --cleanenv --writable-tmpfs --containall \
+singularity shell --cleanenv --writable-tmpfs \
   -B ~/ants_pipelines/data/ANTsLongitudinal/0.1.0/:/data/output \
   ~/ants_pipelines/images/antslongct_0.1.0.sif
 
 # Run singularity container.
-singularity run --cleanenv --writable-tmpfs --containall \
+singularity run --cleanenv --writable-tmpfs \
   -B ~/ants_pipelines/data/ANTsLongitudinal/0.1.0/:/data/output \
   ~/ants_pipelines/images/antslongct_0.1.0.sif --seed 1 --project ExtraLong sub-93811
