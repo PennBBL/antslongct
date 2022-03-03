9856dd393861e8c784361054caa8c0c6a59e452c
katjz <katjazoner@gmail.com>
2022-03-03 09:59:46 -0500

    Fix var name in singularity bind

--- examples/run_singularity.sh
+++ examples/run_singularity.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-VER=$1	# If desired add version suffix to logfiles/jobscript
-
 export LOGS_DIR=/home/kzoner/logs/ExtraLong_2021/ANTsLongCT-0.1.0
 mkdir -p ${LOGS_DIR}
 
-scripts="/project/ExtraLong/scripts/process/datafreeze-2021/ANTsLongitudinal"
+scripts="/project/ExtraLong/scripts/datafreeze-2021/process/ANTsLongitudinal"
 jsDir=${scripts}/jobscripts/ANTsLongCT-0.1.0
 mkdir -p ${jsDir}
 
@@ -13,7 +11,7 @@ data="/project/ExtraLong/data/datafreeze-2021"
 antslong_dir=${data}/ANTsLongitudinal
 
 include_csv=${data}/QC/sessions_for_inclusion.csv
-subList=$(cat ${include_csv} | cut -d , -f 1 | uniq)
+subList=$(cat ${include_csv} | cut -d , -f 1 | uniq | tail -n +2)
 echo "ANTsLongCT will be run on $(echo $subList | wc -w) subjects"
 
 for subject in $subList; do
@@ -27,11 +25,12 @@ for subject in $subList; do
 		#!/bin/bash
 
 		singularity run --writable-tmpfs --cleanenv  \\
-			-B ${out_dir}:/data/output \\
-			/project/ExtraLong/images/antslongct_0.1.0.sif --seed 1 --project ExtraLong ${subject}
+			-B ${antslong_dir}:/data/output \\
+			/project/ExtraLong/images/antslongct_0.1.0.sif --seed 1 --project ExtraLong ${subject} -m 1 -m 2 -m 3
 
 	JOBSCRIPT
 
 	chmod +x ${jobscript}
 	bsub -e $LOGS_DIR/${subject}.e -o $LOGS_DIR/${subject}.o ${jobscript}
+	
 done
